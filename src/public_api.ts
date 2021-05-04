ccc8059c4b46e0d85d9fb99ee0feb3d0e01846cc
Daan van Vugt <daan@vanvugt.me>
2021-05-04 15:03:23 +0200

    Fix rollup export

--- src/public_api.ts
+++ src/public_api.ts
@@ -1,7 +1,7 @@
 /* tslint:disable:file-name-casing */
 export * from './ngx-jsonapi.module';
 /* tslint:enable:file-name-casing */
-export * from 'interfaces';
+export * from './interfaces/index';
 export { Autoregister } from './decorators/autoregister';
 export { Core as JsonapiCore } from './core';
 export { Resource } from './resource';
